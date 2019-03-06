d30a404a1cb39913118e4ddfc9417684dcd7ee83
Павел Бабанов <pavelbabanov@Pavel-Babanovs-MacBook-Pro.local>
2019-03-06 23:04:09 +0300

    Updates

---
+++ precache-manifest.b4272b11d37c64fe28e8ddcb69ad7d22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d0aafabf7c742c629b2",
+    "url": "/mvp1.0/static/js/runtime~main.1d0aafab.js"
+  },
+  {
+    "revision": "721c298eb98ee0b8810a",
+    "url": "/mvp1.0/static/js/main.721c298e.chunk.js"
+  },
+  {
+    "revision": "434c5556e87c0dfa0b07",
+    "url": "/mvp1.0/static/js/2.434c5556.chunk.js"
+  },
+  {
+    "revision": "721c298eb98ee0b8810a",
+    "url": "/mvp1.0/static/css/main.c4cc5e6d.chunk.css"
+  },
+  {
+    "revision": "84acb339bd2c63bf80f573832e7020b1",
+    "url": "/mvp1.0/index.html"
+  }
+];
